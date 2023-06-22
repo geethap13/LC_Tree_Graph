82a56e324428e0a96adc55d73e5876efecd13dad
Geetha <104586263+geethap13@users.noreply.github.com>
2023-06-22 20:35:28 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.cutRod(arr, n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public int cutRod(int price[], int N) {
+        //code here
+        
+    int dp[][]=new int[N][N+1];
+    
+    for(int row[]:dp)
+    Arrays.fill(row,-1);
+    
+    for(int i=0; i<=N; i++){
+        dp[0][i] = i*price[0];
+    }
+    
+    for(int ind=1; ind<N; ind++){
+        for(int length =0; length<=N; length++){
+        
+             int notTaken = 0 + dp[ind-1][length];
+    
+             int taken = Integer.MIN_VALUE;
+             int rodLength = ind+1;
+             if(rodLength <= length)
+                taken = price[ind] + dp[ind][length-rodLength];
+        
+             dp[ind][length] = Math.max(notTaken,taken);   
+        }
+    }
+    
+    return dp[N-1][N];
+    }
+}
